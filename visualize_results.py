9326c284d40ecb28ab8898602f2e3dc4b18dfc83
Yuvashree R <yuvashree.ra05@gmail.com>
2026-07-26 12:59:37 +0530

    Update visualize_results.py

--- visualize_results.py
+++ visualize_results.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 from energy_prediction_models import EnergyPredictionModels
 
-#import
 def create_visualizations():
     plt.style.use('seaborn-v0_8')
     sns.set_palette("husl") #hus1 
